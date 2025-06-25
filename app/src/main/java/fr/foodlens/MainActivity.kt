7adfefba4c4e4db29bff11510c75c1e4f1d61f3c
David Marembert <d0gma@d0gma.fr>
2025-06-25 16:26:44 +0200

    Add buttons in main activity UI

--- app/src/main/java/fr/foodlens/MainActivity.kt
+++ app/src/main/java/fr/foodlens/MainActivity.kt
@@ -1,6 +1,7 @@
 package fr.foodlens
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val shoppingListButton = findViewById<Button>(R.id.shoppingListButton)
+        val recipeButton = findViewById<Button>(R.id.recipeButton)
+        val fridgeButton = findViewById<Button>(R.id.fridgeButton)
+
+        shoppingListButton.requestFocus()
     }
 }
